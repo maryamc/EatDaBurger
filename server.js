4d9e9957afcd42a21504c0a329938f4d2a7a433a
Maryam Chaudhry <maryamc1798@gmail.com>
2020-12-03 19:30:09 -0500

    setting up server js and installing npm packages

---
+++ server.js
@@ -0,0 +1,15 @@
+// Dependencies
+var express = require("express");
+
+// setting up express app
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// setting up the express app to handle the data parsing
+app.use(express.urlencoded({extended: true}));
+appendFile,use(express.json());
+
+// adding server listener
+app.listen(PORT, function(){
+    console.log ("Server listening on: http://localhost:" + PORT);
+});
