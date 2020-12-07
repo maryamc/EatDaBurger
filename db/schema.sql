c200ad1b952160b700f629dfc7cfbc7eb44dd18e
Maryam Chaudhry <maryamc1798@gmail.com>
2020-12-06 20:07:56 -0500

    creating database in schema

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id)
+);
+
+
+
+-- * Create the `burgers_db`.
+--    * Switch to or use the `burgers_db`.
+--    * Create a `burgers` table with these fields:
+--      * **id**: an auto incrementing int that serves as the primary key.
+--      * **burger_name**: a string.
+--      * **devoured**: a boolean.
