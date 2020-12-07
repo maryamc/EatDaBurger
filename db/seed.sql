5a648e9ff886602ef105ae5f07a69b5c1523dd96
Maryam Chaudhry <maryamc1798@gmail.com>
2020-12-06 20:17:33 -0500

    adding insert values into seeds.sql

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Filet O Fish");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Big Mac");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Quarter Pounder")
