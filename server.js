455c76b864633ea5d9c90d9d5966867fdd2f413d
Maryam Chaudhry <maryamc1798@gmail.com>
2021-02-23 15:34:28 -0500

    adding previously missed routes to server page

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var express = require("express");
 // setting up express app
 var app = express();
 var PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
+
 
 // setting up the express app to handle the data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+//connecting routes to server
+var routes = require("./controllers/burger_controllers");
+app.use(routes);
 
 // adding server listener
 app.listen(PORT, function(){
